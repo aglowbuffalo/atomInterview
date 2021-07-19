2338d47958be2ff2a5829c76a70636b0f323f192
aglowbuffalo <popcorn44@abv.bg>
2021-07-19 09:28:31 +0300

    Signed-off-by: aglowbuffalo <popcorn44@abv.bg>

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -57,10 +57,15 @@
                 {{ $message }}
             @enderror
             </div>
+            <div id="form-text" class="bg-green-700 mb-3 text-white p-1 text-center rounded hidden"></div>
 
             <div>
-                <button type="submit" id="contact-submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full hover:bg-green-700">
-                    Submit
+                <button type="submit" id="contact-submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full relative hover:bg-green-700     ">
+                    <svg id="button-loader" class="invisible animate-spin -ml-1 mr-3 h-5 w-5 text-white absolute mx-auto  left-1/2 h-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                      </svg>
+                    <span id="text-button" >Submit</span>
                 </button>
             </div>
 
@@ -83,6 +88,13 @@
         $('#message').val('');
     }
 
+    function loader(){
+       $('#button-loader').toggleClass('invisible');
+       $('#text-button').toggleClass('invisible');
+       $('form-text').toggleClass('hidden');
+
+    }
+
     var flag = 0;
 
     $("#contact-submit").click(function(e){
@@ -93,6 +105,8 @@
         }
 
         e.preventDefault();
+        loader();
+        $('#form-text').addClass('hidden');
 
         name = $('#name').val();
         email = $('#email').val();
@@ -115,13 +129,17 @@
                 message:message,
             },
             success:function(response) {
-                alert(response.success);
+                // alert(response.success);
                 resetErrors();
                 resetForm();
+                loader();
+                $('#form-text').removeClass('hidden');
+                $('#form-text').text(response.success);
                 flag = 0;
             },
             error:function (response) {
                 resetErrors();
+                loader();
                 $('#nameError').text(response.responseJSON.errors.name);
                 $('#emailError').text(response.responseJSON.errors.email);
                 $('#phoneError').text(response.responseJSON.errors.phone);
